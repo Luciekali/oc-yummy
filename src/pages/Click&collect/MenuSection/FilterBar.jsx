a8135a13248fad82015b70d031017836f954fca4
Lucie Kalinowski <lucie.kalinowski@gmail.com>
2024-02-22 15:11:42 +0100

    only filterbar  style with focus temporary solution

--- src/pages/Click&collect/MenuSection/FilterBar.jsx
+++ src/pages/Click&collect/MenuSection/FilterBar.jsx
@@ -9,14 +9,13 @@ const categories = menu.reduce(
 
 
 function FilterBar({ setCat }) {
-const [activeCat, setActive] = useState(false)
+
 
     return (
             <ul className='filterbar'>
                 {categories.map((cat, index) => (
                     <li  key={`${cat}-${index}`}>
-                        <button  className='filterbar-btn' value={cat} activeCat={activeCat} onClick={(e, button) => {{{setCat(e.target.value)}}; {setActive(e.target === true)}; {
-activeCat === true? li.button.classList.add('active') : li.button.classList.remove('active')}}}>{cat}</button>
+                        <button  className='filterbar-btn' value={cat} onClick={(e) => {setCat(e.target.value)}}>{cat}</button>
                 </li>
                 ))}
                 
