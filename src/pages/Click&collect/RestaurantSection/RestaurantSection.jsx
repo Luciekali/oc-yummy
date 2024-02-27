042a45a6050e51155e06c3ce4e91f3526cbfd234
Lucie Kalinowski <lucie.kalinowski@gmail.com>
2024-02-27 13:29:02 +0100

    right radio button checked

--- src/pages/Click&collect/RestaurantSection/RestaurantSection.jsx
+++ src/pages/Click&collect/RestaurantSection/RestaurantSection.jsx
@@ -1,14 +1,15 @@
 import { restaurant } from "../../../datas/restaurant"
 import '../../../styles/pages/clickandcollect/restaurantsection.scss'
+import { useState } from "react"
 
 function RestaurantSection () {
+  const [ restauCheck, setRestau] = useState(false)    
 return (
     <section className="restaurant-section">
-        <h2>Selectionnez votre restaurant</h2>  
-        
+        <h2>Selectionnez votre restaurant</h2> 
        <ul className="restaurant-list">
+
             {restaurant.map((restau) => (
-                
                 <li className='restaurant-card'key={restau.id}>
                     <label className="restaurant-infos">
                         <div>
@@ -17,14 +18,13 @@ return (
                         <p>{restau.city}</p>
                         <p>{restau.open}h à {restau.close}h</p>
                         </div>
-               <input className='radio-btn' type='radio' value={`${restau.name}`}/></label></li>
-               
-                
+                        <input className='radio-btn' name='restaurant' value={`${restau.name}`} type='radio' 
+                        checked={restauCheck} onChange={e => {setRestau(e.target.checked)}}/>
+                    </label>
+                </li>               
        ))}
-      
        </ul> 
     </section>
-)
-}
+)}
 
 export default RestaurantSection
